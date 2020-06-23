04789099265265669e21a5bed016581365564a20
gcampbell391 <gcampbell391@gmail.com>
2020-06-23 17:08:31 -0400

    user and task endpoints created

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,84 +1,11 @@
 const mongoose = require('mongoose')
-const connectionURL = 'mongodb://127.0.0.1:27017/task-manager-api'
-const validator = require('validator')
 
-//Connect to db
+
+const connectionURL = 'mongodb://127.0.0.1:27017/task-manager-api'
 mongoose.connect(connectionURL, {
     useNewUrlParser: true,
     useCreateIndex: true
 })
 
-//Create User model
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 7,
-        validate(value) {
-            if (value.toLowerCase().includes('password')) {
-                throw new Error("Password can't contain the word password")
-            }
-        }
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid')
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be a positive number')
-            }
-        }
-    }
-})
 
-//Create Task model
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-})
 
-const newTask = new Task({
-    description: 'Go to the gym and complete leg day'
-})
-newTask.save().then(() => {
-    console.log(newTask)
-}).catch(error => {
-    console.log('Error: ', error)
-})
-
-// const me = new User(
-//     {
-//         name: 'Larry',
-//         email: 'L83@GMAIL.COM',
-//         password: 'pas123',
-//         age: 21
-//     })
-// me.save().then(() => {
-//     console.log(me)
-// }).catch(error => {
-//     console.log('Error: ', error)
-// })
