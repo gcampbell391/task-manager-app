c2abe87f62e27b58a2ba1aca1f2a70d8311fa92e
gcampbell391 <gcampbell391@gmail.com>
2020-06-23 22:50:20 -0400

    1/2 of CRUD set up

--- src/index.js
+++ src/index.js
@@ -8,27 +8,81 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-//Create User API Endpoint
-app.post('/users', (req, res) => {
-    console.log(req.body)
+//Create a single User
+app.post('/users', async (req, res) => {
     const user = new User(req.body)
-
-    user.save().then(() => {
+    try {
+        await user.save()
         res.status(201).send(user)
-    }).catch((e) => {
+    }
+    catch (e) {
         res.status(400).send(e)
-    })
+
+    }
 })
 
-app.post('/tasks', (req, res) => {
-    console.log(req.body)
-    const task = new Task(req.body)
+//Read all Users
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({})
+        res.status(202).send(users)
+    }
+    catch (e) {
+        res.status(500).send(e)
+    }
+})
 
-    task.save().then(() => {
-        res.status(201).send(task)
-    }).catch((e) => {
+//Read a single User
+app.get('/users/:id', async (req, res) => {
+    const _id = req.params.id
+    try {
+        const user = await User.findById(_id)
+        if (!user) {
+            return res.status(404).send('User not found')
+        }
+        res.status(202).send(user)
+    }
+    catch (e) {
+        res.status(500).send()
+    }
+})
+
+//Create a single Task
+app.post('/tasks', async (req, res) => {
+    const task = new Task(req.body)
+    try {
+        await task.save()
+        res.status(200).send(task)
+    }
+    catch (e) {
         res.status(400).send(e)
-    })
+    }
+})
+
+//Read all Tasks
+app.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        res.status(200).send(tasks)
+    }
+    catch (e) {
+        res.status(500).send()
+    }
+})
+
+//Read a single Task
+app.get('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+    try {
+        const task = await Task.findById(_id)
+        if (!task) {
+            return res.status(404).send('Task not found')
+        }
+        res.status(200).send(task)
+    }
+    catch (e) {
+        res.status(500).send()
+    }
 })
 
 app.listen(port, () => {
