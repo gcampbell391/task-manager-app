69cf150d4bac15c85eb1ed7b1c60224d1b165b0c
gcampbell391 <gcampbell391@gmail.com>
2020-06-25 18:08:47 -0400

    Back end is set up 90%...user-task relationship est.

--- src/routers/user.js
+++ src/routers/user.js
@@ -55,7 +55,7 @@ router.post('/users/logoutall', auth, async (req, res) => {
     }
 })
 
-//Read all users
+//Read user profile
 router.get('/users/me', auth, async (req, res) => {
     try {
         res.send(req.user)
@@ -65,38 +65,20 @@ router.get('/users/me', auth, async (req, res) => {
     }
 })
 
-//Read user
-router.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
-    try {
-        const user = await User.findById(_id)
-        if (!user) {
-            return res.status(404).send('User not found')
-        }
-        res.status(202).send(user)
-    }
-    catch (e) {
-        res.status(500).send()
-    }
-})
 
 //Update user
-router.patch('/users/:id', async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ["name", "email", "password", "age"]
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
     if (!isValidOperation) {
         return res.status(400).send({ error: 'Invalid updates!' })
     }
-    const _id = req.params.id
     try {
-        const user = await User.findById(_id)
-        updates.forEach((update) => user[update] = req.body[update])
-        await user.save()
-        if (!user) {
-            return res.status(404).send()
-        }
-        res.status(200).send(user)
+        updates.forEach((update) => req.user[update] = req.body[update])
+        await req.user.save()
+        res.status(200).send(req.user)
     }
     catch (e) {
         res.status(400).send(e)
@@ -104,13 +86,10 @@ router.patch('/users/:id', async (req, res) => {
 })
 
 //Delete user
-router.delete('/users/:id', async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
     try {
-        const user = await User.findByIdAndDelete(req.params.id)
-        if (!user) {
-            return res.status(404).send()
-        }
-        res.status(200).send(user)
+        await req.user.remove()
+        res.send(req.user)
     }
     catch (e) {
         res.status(500).send()
