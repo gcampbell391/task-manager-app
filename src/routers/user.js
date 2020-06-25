a01c18b39307a1e55b068a1872180e1a0a7c19d5
gcampbell391 <gcampbell391@gmail.com>
2020-06-24 21:42:56 -0400

    auth 1/2 complete

--- src/routers/user.js
+++ src/routers/user.js
@@ -7,12 +7,24 @@ const User = require('../models/user')
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
     try {
+        const token = await user.generateAuthToken()
         await user.save()
-        res.status(201).send(user)
+        res.status(201).send({ user, token })
     }
     catch (e) {
         res.status(400).send(e)
+    }
+})
 
+//User log in
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
+    }
+    catch (e) {
+        res.status(400).send()
     }
 })
 
@@ -52,7 +64,9 @@ router.patch('/users/:id', async (req, res) => {
     }
     const _id = req.params.id
     try {
-        const user = await User.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true })
+        const user = await User.findById(_id)
+        updates.forEach((update) => user[update] = req.body[update])
+        await user.save()
         if (!user) {
             return res.status(404).send()
         }
